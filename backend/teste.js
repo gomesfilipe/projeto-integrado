8aed792c4237026920e50f1e4783ab47347b93af
gomesfilipe <filipejvmf@gmail.com>
2022-06-12 13:09:40 -0300

    Testes com datas.

--- backend/teste.js
+++ backend/teste.js
@@ -1,9 +1,18 @@
-var vetor = ['a', 'b', 'c', 'd']
+// var vetor = ['a', 'b', 'c', 'd']
 
-var index = vetor.indexOf('d')
+// var index = vetor.indexOf('d')
 
-console.log(index)
+// console.log(index)
 
-vetor.splice(index, 1)
+// vetor.splice(index, 1)
 
-console.log(vetor)
+// console.log(vetor)
+
+var date = new Date()
+
+console.log(date)
+console.log(date.toISOString())
+var date2 = new Date('2021-01-01')
+console.log(date2.toISOString())
+
+console.log(date2)
