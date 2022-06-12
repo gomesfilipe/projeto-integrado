7fd9eb45a4b3b2bbcc2d01a82be2cde3c51d876b
gomesfilipe <filipejvmf@gmail.com>
2022-06-12 20:04:08 -0300

    Middleware para controlar as rotas que só podem acessar quando está logado feito.

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken')
+const auth_config = require('../config/auth.json')
+
+module.exports = (req, res, next) => {
+    const auth_header = req.headers.authorization
+    
+    if(!auth_header) {
+        return res.json({err: "Token não fornecido."})
+    }
+
+    const parts = auth_header.split(' ')
+    if(!(parts.length === 2)) {
+        return res.json({err: "Token inválido."})
+    }
+    
+    const [scheme, token] = parts
+    
+    if(!(/^(Bearer)$/i.test(scheme))) {
+        return res.json({err: "Token mal-formatado."})
+    }
+
+    jwt.verify(token, auth_config.secret, (err, decoded) => {
+        if (err) {
+            return res.json({err: 'Token inválido.'})
+        }
+
+        req.store_id = decoded.id
+    })
+
+    return next()
+}
