6577dbfd203aa74ff831d279e0afa8dd6ad14eee
gomesfilipe <filipejvmf@gmail.com>
2022-07-04 17:57:16 -0300

    TODO atualizado, configuração do Swagger adicionada.

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,35 @@
 const express = require('express')
-const app = express()
-const store = require('./routes/store')
-const product = require('./routes/product')
-const sale = require('./routes/sale')
-const item = require('./routes/item')
-const nologged = require('./routes/no_logged')
+// const app = express()
+const app = require('./routes')
+// const store = require('./routes/store')
+// const product = require('./routes/product')
+// const sale = require('./routes/sale')
+// const item = require('./routes/item')
+// const nologged = require('./routes/no_logged')
 const mongoose = require('mongoose')
+
+const swaggerJsdoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+
+const port = 5500
+
+// Configuração Swagger
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            openapi: '3.0.0',
+            title: 'SISVE API',
+            description: 'API para desenvolvimento do projeto SISVE',
+            version: '1.0.0'
+        },
+        servers: [`http://localhost:${port}`]
+    },
+    apis: ['routes/item.js', 'routes/no_logged.js', 'routes/product.js', 'routes/sale.js', 'routes/store.js']
+}
+
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 // Configuração mongoose.
 // mongoose.Promise = global.Promise
@@ -14,20 +38,22 @@ mongoose.connect('mongodb://localhost/sisve')
     .catch(() => console.log('Error to connect database.'))
 
 // Rodando servidor.
-const port = 8081
+// const port = 8081
 app.listen(port, () => {
     console.log(`The server is running in http://localhost:${port}`)
 })
 
-// Middleware.
-app.use(express.json())
+// // Middleware.
+// app.use(express.json())
 
 // Usando rotas user.
-app.use('/store', store) // /store é o prefixo para acessar as rotas desse grupo.
-app.use('/product', product) // /product é o prefixo para acessar as rotas desse grupo.
-app.use('/sale', sale) // /sale é o prefixo para acessar as rotas desse grupo.
-app.use('/item', item) // /item é o prefixo para acessar as rotas desse grupo.
-app.use('/nologged', nologged) // /nologged é o prefixo para acessar as rotas desse grupo.
+// app.use('/store', store) // /store é o prefixo para acessar as rotas desse grupo.
+// app.use('/product', product) // /product é o prefixo para acessar as rotas desse grupo.
+// app.use('/sale', sale) // /sale é o prefixo para acessar as rotas desse grupo.
+// app.use('/item', item) // /item é o prefixo para acessar as rotas desse grupo.
+// app.use('/nologged', nologged) // /nologged é o prefixo para acessar as rotas desse grupo.
+
+// module.exports = app // Exportando app para poder executar os testes automatizados.
 
 // TODO
 // Ajeitar chaves de busca para produtos e lojas. (De preferência IDs) //!OK
@@ -47,6 +73,15 @@ app.use('/nologged', nologged) // /nologged é o prefixo para acessar as rotas d
 // Definir critérios de validação de username, password e admin_password.
 // Documentar para facilitar o desenvolvimento do front-end. (Swagger).
 // Fazer testes das validações já feitas (de parâmetros vindos do req.body e dos parâmetros numéricos).
+// Fazer scripts de testes.
+// Ver documentação do describe.
+// Mudar implementação da rota de deletar loja (deve excluir os produtos e vendas associadas a ela).
+// Fazer verificação se loja existe no cadastro de produtos.
+// Remover campo admin_password da model Store.
+// Ver como fazer upload de imagens (ou remover este campo).
+// No cadastro de produto deve verificar se o id da loja existe.
+// No cadastro de item deve verificar se o id da loja e produto existem.
+// Colocar middleware para verificar se o usuário logado está fazendo ações na própria loja.
 
 // TODO TESTES
 // Testes CRUD Stores //!OK
