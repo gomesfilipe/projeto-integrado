df01867880057088afd32142a004264ee97de6db
gomesfilipe <filipejvmf@gmail.com>
2022-07-04 17:56:43 -0300

    Arquivo para gerenciar grupos de rotas.

---
+++ backend/routes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const store = require('./routes/store')
+const product = require('./routes/product')
+const sale = require('./routes/sale')
+const item = require('./routes/item')
+const nologged = require('./routes/no_logged')
+
+// Middleware.
+app.use(express.json())
+
+app.use('/store', store) // /store é o prefixo para acessar as rotas desse grupo.
+app.use('/product', product) // /product é o prefixo para acessar as rotas desse grupo.
+app.use('/sale', sale) // /sale é o prefixo para acessar as rotas desse grupo.
+app.use('/item', item) // /item é o prefixo para acessar as rotas desse grupo.
+app.use('/nologged', nologged) // /nologged é o prefixo para acessar as rotas desse grupo.
+
+module.exports = app
