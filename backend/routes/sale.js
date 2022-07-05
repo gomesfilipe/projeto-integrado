3beb7bd48085bfaaf9eab1f369f2441f5a7d2a67
gomesfilipe <filipejvmf@gmail.com>
2022-07-04 21:37:17 -0300

    Documentação com Swagger.

--- backend/routes/sale.js
+++ backend/routes/sale.js
@@ -14,10 +14,39 @@ const auth_middleware = require('../middlewares/auth')
 
 router.use(auth_middleware) // Middleware atuará nas rotas desse grupo.
 
+/**
+ * @swagger
+ * securityDefinitions:
+ *     Bearer:
+ *       description: Efetue login para obter o token de autenticação.
+ *       type: apiKey
+ *       name: Authorization
+ *       in: header
+ */
+
+/**
+ * @swagger
+ * /sale/api/all:
+ *      get:
+ *          summary: Busca de todas as vendas de todas as lojas cadastradas.
+ *          description: Rota para consultar todas as vendas efetuadas.
+ *                       É necessário estar logado para acessá-la.
+ *          tags: [Sale]
+ *          security:
+ *            - Bearer: []
+ * 
+ *          responses: 
+ *              '200': 
+ *                  description: Produtos consultados com sucesso!
+ *              '400':
+ *                  description: Erro ao consultar produtos no banco de dados.
+ *              '401':
+ *                  description: Token inválido.
+ */
 router.get('/api/all', (req, res) => {
     Sale.find()
-        .then(sales => res.json({sales}))
-        .catch(err => res.json({ message: 'Erro ao buscar vendas.' }))
+        .then(sales => res.status(200).json({sales}))
+        .catch(err => res.status(400).json({ message: 'Erro ao buscar vendas.' }))
 })
 
 router.get('/api/dates', (req, res) => {
@@ -30,16 +59,52 @@ router.get('/api/dates', (req, res) => {
         "$gte": req.body.from_date, // Início do período. Obs: devem estar no formato ISO.
         "$lt": req.body.to_date // Fim do período. Exemplo: "2022-06-12T14:49:01.686Z"
     }})
-        .then(sales => res.json({sales}))
-        .catch(err => res.json({ message: 'Erro ao buscar vendas.' }))
+        .then(sales => res.status(200).json({sales}))
+        .catch(err => res.status(400).json({ message: 'Erro ao buscar vendas.' }))
 })
 
+/**
+ * @swagger
+ * /sale/api/:
+ *      post:
+ *          summary: Cadastro de venda.
+ *          description: Rota para efetuar as vendas da loja.
+ *                       É necessário estar logado para acessá-la.
+ *          tags: [Sale]
+ *          security:
+ *            - Bearer: []
+ *          parameters:
+ *          - in: body
+ *            name: product
+ *            schema:
+ *              type: object
+ *              properties:
+ *                items:
+ *                  type: array
+ *                  items:
+ *                      type: string
+ *                      example: 62c34b928340ed2117560766
+ *                value:
+ *                  type: string
+ *                  example: 120.00
+ *                id_store:
+ *                  type: string
+ *                  example: 62c2440e7e340b831c3ab807
+ * 
+ *          responses: 
+ *              '200': 
+ *                  description: Venda efetuada com sucesso!
+ *              '400':
+ *                  description: Erro ao cadastrar venda no banco de dados, ou faltam dados, ou dados foram enviados de modo incorreto.
+ *              '401':
+ *                  description: Token inválido.
+ */
 router.post('/api', (req, res) => {
     if(!req.body.items || !req.body.value || !req.body.id_store)
-        return res.json({ message: 'Faltam dados.' })
+        return res.status(400).json({ message: 'Faltam dados.' })
     
     if(isNaN(req.body.value))
-        return res.json({ message: 'Há dados inválidos.'})
+        return res.status(400).json({ message: 'Há dados inválidos.'})
 
 
     const sale = new Sale({
@@ -50,32 +115,58 @@ router.post('/api', (req, res) => {
 
     sale.save()
         .then(() => {
-            Store.findOne({id: req.body.id_store})
+            Store.findOne({_id: req.body.id_store})
                 .then(store => {
                     store.sales.push(sale)
+                    console.log('chegou aqui')
                     store.save()
-                        .then(() => res.json({ message: 'Venda concluída com sucesso!' }))
-                        .catch(err => res.json({ message: 'Erro ao concluir venda.' }))
+                        .then(() => res.status(200).json({ message: 'Venda concluída com sucesso!' }))
+                        .catch(err => res.status(400).json({ message: 'Erro ao concluir venda.' }))
                 })
-                .catch(err => res.json({ message: 'Erro ao concluir venda.' }))
+                .catch(err => res.status(400).json({ message: 'Erro ao concluir venda.' }))
         })
-        .catch(err => res.json({ message: 'Erro ao concluir venda.' }))
+        .catch(err => res.status(400).json({ message: 'Erro ao concluir venda1.' }))
 })
 
+/**
+ * @swagger
+ * /sale/api/{id}:
+ *      delete:
+ *          summary: Exclusão de venda por id.
+ *          description: Rota para deletar uma venda específica por id.
+ *                       É necessário estar logado para acessá-la.
+ *          tags: [Sale]
+ *          security:
+ *            - Bearer: []
+ *          
+ *          parameters:
+ *          - in: path
+ *            name: id
+ *            type: string
+ *            required: true
+ * 
+ *          responses: 
+ *              '200': 
+ *                  description: Produto deletado com sucesso!
+ *              '400':
+ *                  description: Erro ao deletar venda no banco de dados ou venda não encontrada.
+ *              '401':
+ *                  description: Token inválido.
+ */
 router.delete('/api/:id', (req, res) => {
     const sale_id = req.params.id
 
     Sale.findOne({_id: sale_id})
         .then(sale => {
             if(!sale) {
-                return res.json({ message: 'Venda inexistente.' })
+                return res.status(400).json({ message: 'Venda inexistente.' })
             } else {
                 Sale.deleteOne({_id: sale_id})
-                    .then(() => res.json({ message: 'Venda deletada com sucesso!' }))
-                    .catch(err => res.json({ message: 'Erro ao deletar venda.' }))
+                    .then(() => res.status(200).json({ message: 'Venda deletada com sucesso!' }))
+                    .catch(err => res.status(400).json({ message: 'Erro ao deletar venda.' }))
             }
         })
-        .catch(err => res.json({ message: 'Erro ao deletar venda.' }))
+        .catch(err => res.status(400).json({ message: 'Erro ao deletar venda.' }))
 })
 
 module.exports = router
