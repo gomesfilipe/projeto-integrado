7b3e5f3a26c23358cad69f01cba933c54465e350
gomesfilipe <filipejvmf@gmail.com>
2022-07-09 19:54:56 -0300

    Alguns comentários removidos.

--- backend/routes/item.js
+++ backend/routes/item.js
@@ -19,6 +19,9 @@ const auth_middleware = require('../middlewares/auth')
  *       type: apiKey
  *       name: Authorization
  *       in: header
+ * 
+ * definitions:
+ * 
  */
 
 /**
@@ -27,6 +30,7 @@ const auth_middleware = require('../middlewares/auth')
  *      get:
  *          summary: Busca de todos os itens de todas as lojas cadastradas.
  *          description: Rota para consultar todos os itens cadastrados.
+ *                       Não é necessário autenticação para acessá-la.
  *          tags: [Item]
  * 
  *          responses: 
@@ -67,98 +71,5 @@ router.get('/api', auth_middleware, (req, res) => {
         .then(items => res.status(200).json({ items }))
         .catch(err => res.status(400).json({ message: 'Erro ao buscar itens.' }))
 })
-
-/**
- * swagger
- * /item/api:
- *      post:
- *          summary: Cadastro de item.
- *          description: Rota para efetuar cadastro de item, devendo ser informados
- *                       id do produto, quantidade e id de sua loja.
- *                       É necessário estar logado para acessá-la.
- *          tags: [Item]
- *          security:
- *            - Bearer: []
- *          parameters:
- *          - in: body
- *            name: item
- *            schema:
- *              type: object
- *              properties:
- *                id_product:
- *                  type: string
- *                  example: 62c339f67ef69ce43d463525
- *                quantity:
- *                  type: string
- *                  example: 3
- * 
- *          responses: 
- *              '200': 
- *                  description: Item cadastrado com sucesso!
- *              '400':
- *                  description: Erro ao cadastrar item. (Faltaram dados no corpo da requisição,
- *                               ou ocorreu falha ao salvar no banco de dados ou 
- *                               dados foram passados de modo incorreto)
- *              '401':
- *                  description: Token inválido.
- */
-// router.post('/api', (req, res) => {
-//     if(!req.body.id_product || /*!req.body.id_store ||*/ !req.body.quantity)
-//         return res.status(400).json({ message: 'Faltam dados.' })
-    
-//     if(isNaN(req.body.quantity))
-//         return res.status(400).json({ message: 'Há dados inválidos.'})
-        
-//     const item = new Item({
-//         id_product: req.body.id_product,
-//         id_store: req.store_id, // Campo store_id da requisição vem do middleware de autenticação.
-//         // id_store: req.body.id_store,
-//         quantity: Number(req.body.quantity)
-//     })
-
-//     item.save()
-//         .then(() => res.status(200).json({ message: 'Item criado com sucesso!' }))
-//         .catch(err => res.status(400).json({ message: 'Erro ao criar item.' }))
-// })
-
-/**
- * swagger
- * /item/api/{id}:
- *      delete:
- *          summary: Exclusão de item por id.
- *          description: Rota para deletar item por id.
- *                       É necessário estar logado para acessá-la.
- *          tags: [Item]
- *          security:
- *            - Bearer: []
- *          parameters:
- *          - in: path
- *            name: id
- *            type: string
- *            required: true
- * 
- *          responses: 
- *              '200': 
- *                  description: Item deletado com sucesso!
- *              '400':
- *                  description: Erro ao deletar item no banco de dados ou item não existe.
- *              '401':
- *                  description: Token inválido.
- */
-// router.delete('/api/:id', (req, res) => {
-//     const item_id = req.params.id
-
-//     Item.findOne({_id: item_id})
-//         .then(item => {
-//             if(!item) {
-//                 return res.status(400).json({ message: 'Item inexistente.' })
-//             } else {
-//                 Item.deleteOne({id: item_id})
-//                     .then(() => res.status(200).json({ message: 'Item deletado com sucesso!' }))
-//                     .catch(err => res.status(400).json({ message: 'Erro ao deletar item.' }))
-//             }
-//         })
-//         .catch(err => res.status(400).json({ message: 'Erro ao deletar item.' }))
-// })
 
 module.exports = router
