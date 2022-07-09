abee46f680127deed8c77e7b240447c031a42904
gomesfilipe <filipejvmf@gmail.com>
2022-07-09 14:01:53 -0300

    Middleware para rotas que não precisam de autenticação, mas que não podem ser acessadas quando está logado.

---
+++ backend/middlewares/no_auth.js
@@ -0,0 +1,9 @@
+module.exports = (req, res, next) => {
+    const auth_header = req.headers.authorization
+
+    if(auth_header != undefined) {
+        return res.status(401).json({err: "Proibido acessar esta rota estando logado."})
+    }
+
+    return next()
+}
