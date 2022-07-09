c96babe1cba58a676e3c6c8838f321a47cc78ec9
gomesfilipe <filipejvmf@gmail.com>
2022-07-09 19:55:13 -0300

    Mensagem de erro corrigida.

--- backend/middlewares/no_auth.js
+++ backend/middlewares/no_auth.js
@@ -2,7 +2,7 @@ module.exports = (req, res, next) => {
     const auth_header = req.headers.authorization
 
     if(auth_header != undefined) {
-        return res.status(401).json({err: "Proibido acessar esta rota estando logado."})
+        return res.status(401).json({err: "Proibido acessar esta rota logado."})
     }
 
     return next()
