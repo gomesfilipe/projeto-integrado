b0657d7bf6980e2846dbd6938e59f2a79fcd46d2
gomesfilipe <filipejvmf@gmail.com>
2022-07-10 16:29:13 -0300

    Alguns testes feitos.

--- backend/teste.js
+++ backend/teste.js
@@ -8,14 +8,24 @@
 
 // console.log(vetor)
 
-// var date = new Date()
+var date = new Date()
 
 // console.log(date)
 // console.log(date.toISOString())
-// var date2 = new Date('2021-01-01')
+var date2 = new Date('2021-02-30 00:00:00')
 // console.log(date2.toISOString())
 
-// console.log(date2)
+console.log('dia: ' + date2.getDate())
+console.log('mes: ' + date2.getMonth())
+console.log('ano: ' + date2.getFullYear())
+
+if(date2 == 'Invalid Date') {
+    console.log('data invalida')
+} else{
+    console.log('data valida')
+}
+
+// console.log(date2.getMonth())
 
 // var vetor = [{nome: 'teste'},{nome: 'testando'}]
 // console.log(typeof(vetor))
@@ -26,8 +36,8 @@
 //     console.log('nao entrou')
 // }
 
-console.log(isNaN('1'))
-console.log(isNaN('-1'))
-console.log(isNaN('-1.3'))
-console.log(isNaN('1.5'))
-console.log(isNaN('a'))
+// console.log(isNaN('1'))
+// console.log(isNaN('-1'))
+// console.log(isNaN('-1.3'))
+// console.log(isNaN('1.5'))
+// console.log(isNaN('a'))
