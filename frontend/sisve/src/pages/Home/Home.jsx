0d1e294bec95e359bbd26c6bd00ab8d7357bee1e
elainedias16 <elainediaspires16@gmail.com>
2022-07-18 23:13:24 -0300

    Começando fontend

---
+++ frontend/sisve/src/pages/Home/Home.jsx
@@ -0,0 +1,38 @@
+import './Home.css'
+
+function Home() {
+  
+  return (
+
+    <div className="container">
+      <h1>SISVE</h1>
+      <button type="button">Cadastrar</button>
+      <p>Cadastre sua loja!</p>
+    </div>
+
+    // <div className="Home">
+    //   <div>
+    //     <a href="https://vitejs.dev" target="_blank">
+    //       <img src="/vite.svg" className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://reactjs.org" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   <h1>SISVE</h1>
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count is {count}
+    //     </button>
+    //     <p>
+    //       Edit <code>src/Home.jsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p className="read-the-docs">
+    //     Click on the Vite and React logos to learn more
+    //   </p>
+    // </div>
+  )
+}
+
+export default Home
