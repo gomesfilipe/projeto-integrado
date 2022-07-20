2dc9ab85c2d937597806014f908f0734da40a6aa
elainedias16 <elainediaspires16@gmail.com>
2022-07-19 23:34:34 -0300

    fazendo a tela de login

--- frontend/sisve/src/pages/Home/Home.jsx
+++ frontend/sisve/src/pages/Home/Home.jsx
@@ -3,11 +3,52 @@ import './Home.css'
 function Home() {
   
   return (
+    
+    // <div className="container">
+    //   <h1>SISVE</h1>
+    //   <p>Sistema de Vendas e Estoque</p>
 
-    <div className="container">
-      <h1>SISVE</h1>
-      <button type="button">Cadastrar</button>
-      <p>Cadastre sua loja!</p>
+    //   {/* <button>Username</button>
+    //   <button>Senha</button> */}
+
+    //   <input type="text" name="Username" />
+    //   <input type="text" name="Senha" />
+
+    //   {/* <p>Cadastre sua loja!</p> */}
+    //   {/* <button type="button">Cadastrar</button> */}
+    //   {/* <style>
+    //     @import url('https://fonts.googleapis.com/css2?family=Limelight&display=swap');
+    //   </style> */}
+
+
+    // </div>
+
+    
+
+    <div>
+
+      <div className="cadastro">
+      <p>Ainda não possui uma conta?</p>
+        <button>Cadastra-se</button> 
+      </div>
+
+      <div className="container">
+        <h1>SISVE</h1>
+        <p>Sistema de Vendas e Estoque</p>
+      </div>
+
+      <div className='Login'>
+        <h3>Login</h3>
+        <p>Username</p>
+        <input type="text" name="Username" />
+
+        <p>Senha</p>
+        <input type="text" name="Senha" />
+        <button>Login</button>
+        {/* <p>Ainda não possui uma conta?</p>
+        <button>Cadastra-se</button> */}
+      </div>
+
     </div>
 
     // <div className="Home">
