20c6d214906609a3511cdc3d01a4882e33f9ee05
Juliana Repossi <julianarepossi@hotmail.com>
2022-07-22 19:04:15 -0300

    Definindo React Routes

--- frontend/sisve/src/main.jsx
+++ frontend/sisve/src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './pages/Home/Home'
-/*import Products from './pages/Products/Products';*/
 import './styles/global.css';
+import Rota from './rotas'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/> 
-    {/*<Products></Products>*/}
+    <Rota/> 
   </React.StrictMode>
 )
+ 
---
+++ frontend/sisve/src/pages/Cadastro/Cadastro.jsx
@@ -0,0 +1,13 @@
+import './Cadastro.css'
+
+{/*A senha deve conter mais de 4 chars*/}
+
+function Cadastro() {
+  return ( 
+    <div>
+        Ainda comecarei
+    </div>
+
+  )
+}
+export default Cadastro
