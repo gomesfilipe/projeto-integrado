e9e610201ee387b664aba60960e8bb903cd13c96
elainedias16 <elainediaspires16@gmail.com>
2022-07-22 14:11:59 -0300

    fazendo o menu

--- frontend/sisve/src/main.jsx
+++ frontend/sisve/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home/Home'
 import Products from './pages/Products/Products';
 import './styles/global.css';
--- frontend/sisve/src/pages/Components/Sidebar.jsx
+++ frontend/sisve/src/pages/Components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './Sidebar.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import {
   ProSidebar,
@@ -14,6 +15,10 @@ import {
 import StoreMallDirectoryIcon from '@material-ui/icons/StoreMallDirectory';
 import LocalGroceryStoreIcon from '@material-ui/icons/LocalGroceryStore';
 import LocalMallIcon from '@material-ui/icons/LocalMall';
+
+import { Link } from 'react-router-dom';
+
+import './loja'
 
 
 function Sidebar() {
@@ -30,11 +35,21 @@ function Sidebar() {
                 </SidebarHeader>
                 <SidebarContent>
                     <Menu iconShape="square">
-                        <MenuItem icon= {<StoreMallDirectoryIcon />} >
+                        <MenuItem icon= {<StoreMallDirectoryIcon />}>
                             Loja
+                            {/* <a href="http://https://www.youtube.com/"></a> */}
+                            {/* <link rel="stylesheet" href="https://www.youtube.com/" /> */}
+                            {/* Loja */}
                             {/* <a href="http://localhost:5173/loja"></a> */}
-                        </MenuItem>
+                            {/* <Route path='./loja'>Loja</Route> */}
+                            {/* <Link></Link> */}
 
+                            <div className="sidebar-btn-wrapper">
+                               <a href="https://www.youtube.com/">loja</a>
+                            </div>
+
+                            {/* <Link to="./loja"></Link> */}
+                        </MenuItem>
 
                         <MenuItem icon= {<LocalMallIcon />} >Produtos</MenuItem>
                         <MenuItem icon= {<LocalGroceryStoreIcon />}>Venda</MenuItem>
