f7996d675ca8ca98cd356bf4ab7fabc5cd3066b1
gomesfilipe <filipejvmf@gmail.com>
2022-07-23 15:59:00 -0300

    TODO finalizado. Porta trocada pra 8000.

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const swaggerOptions = {
             openapi: '3.0.0',
             title: 'Sistema de Vendas e Estoque (SISVE) API',
             description: 'Documentação da API para desenvolvimento do projeto SISVE.',
-            version: '2.0.0'
+            version: '3.0.0'
         },
         servers: [`http://localhost:${port}`]
     },
