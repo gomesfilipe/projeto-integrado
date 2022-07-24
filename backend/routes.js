2390e3851f934aa53648e9e5ff886d8ec0bce87e
gomesfilipe <filipejvmf@gmail.com>
2022-07-23 23:24:39 -0300

    Cors adicionado nas rotas.

--- backend/routes.js
+++ backend/routes.js
@@ -4,10 +4,10 @@ const store = require('./routes/store')
 const product = require('./routes/product')
 const sale = require('./routes/sale')
 const item = require('./routes/item')
+const cors = require('cors')
 
-// Middleware.
-app.use(express.json())
-
+app.use(cors()) // Middleware para permitir terceiros enviarem requisições a sua API.
+app.use(express.json()) // Middleware para trabalhar com json nas requisições e respostas.
 app.use('/store', store) // /store é o prefixo para acessar as rotas desse grupo.
 app.use('/product', product) // /product é o prefixo para acessar as rotas desse grupo.
 app.use('/sale', sale) // /sale é o prefixo para acessar as rotas desse grupo.
