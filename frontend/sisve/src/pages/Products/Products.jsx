f9ed87aa301c7db4af27b66cdb0b62a66ba5b746
gomesfilipe <filipejvmf@gmail.com>
2022-08-10 18:35:05 -0300

    CRUD de produtos implementado.

--- frontend/sisve/src/pages/Products/Products.jsx
+++ frontend/sisve/src/pages/Products/Products.jsx
@@ -4,6 +4,10 @@ import './Products.css'
 import Header from "../../Components/Header";
 import {Link} from 'react-router-dom'
 import { FaPlus } from "react-icons/fa";
+import { FaPen } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
+
 
 function Products() {
 
@@ -11,18 +15,33 @@ function Products() {
   const stringName = sessionStorage.getItem('name')
   const name = JSON.parse(stringName)
   const [products, setProducts] = useState([]); 
-
+  const [input, setInput] = useState('')
 
   useEffect(() =>{
     api.get('/product/api')
-   .then( res => {
-      setProducts(res.data.products); //yey
+    .then( res => {
+      setProducts(res.data.products);
     })
-   .catch((error) => {
+    .catch((error) => {
       console.error('err : ' , error);
     })
   }, []);
+  
+  async function apagarProduto(id_product) {
+    await api.delete(`/product/api/${id_product}`)
+    setProducts(products.filter(product => product._id !== id_product))
+    alert('Produto deletado com sucesso!')
+  }
 
+  function searchProduct(product_name){
+    api.get(`/product/api/${product_name}`)
+    .then(res =>{
+      setProducts(res.data.products); 
+    })
+    .catch((error) => {
+      console.error('err : ' , error);
+    })       
+  }
 
   return (     
     <div>        
@@ -31,10 +50,21 @@ function Products() {
 
         <div className='Products'>
           <h1> Todos os produtos da {name}: </h1>
-
+          
           <div className='Botao'>
           <Link to="/Product_registration"> <button>{<FaPlus />}</button> </Link>
-          </div> 
+          </div>
+
+          <div className='Search'>
+              <h5>Buscar Produto</h5>
+              <input type="text" 
+                onChange={(e) =>  {setInput(e.target.value)}}         
+              />
+
+              <button type='button' onClick={(e) => {searchProduct(input)}}>
+                {<FaSearch />}
+              </button>
+          </div>
 
           <table className="table table-hover table-bordered table-striped ">
             <thead>
@@ -45,6 +75,8 @@ function Products() {
                 <th scope="col">Preço</th>
                 <th scope="col">Quantidade</th>
                 <th scope="col">Unidade</th>
+                <th scope="col">Editar</th>
+                <th scope="col">Apagar</th>
               </tr>
             </thead>
             <tbody>
@@ -56,7 +88,14 @@ function Products() {
                     <td> {product.cost}</td>
                     <td> {product.sale}</td>
                     <td> {product.quantity}</td>
-                    <td> {product.unity}</td>      
+                    <td> {product.unity}</td>
+                    <td>
+                      {/* <Link to="/Product_edit/"> <button> {<FaPen/>} </button> </Link>  */}
+                      <Link to={`/Product_edit/${product.name}`}> <button> {<FaPen/>} </button> </Link> 
+                    </td>
+                    <td>
+                      <button onClick={(e) => apagarProduto(product._id)}> {<FaTrash/>} </button>
+                    </td>     
                   </tr> 
               })}
 
