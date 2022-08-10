6a292a3f8bb22af3a815907a0f71fedf5867675e
elainedias16 <elainediaspires16@gmail.com>
2022-08-10 00:07:38 -0300

    fazendo tela de vendas

--- frontend/sisve/src/pages/Vendas/Vendas.jsx
+++ frontend/sisve/src/pages/Vendas/Vendas.jsx
@@ -11,6 +11,7 @@ import { FaSearch } from "react-icons/fa";
 function Vendas() {
 
   const [products, setProducts] = useState([]); 
+  const [pQtd, setpQtd] = useState('');
 
       useEffect(() =>{
         api.get('/product/api')
@@ -29,9 +30,10 @@ function Vendas() {
       <div style={{ display: "flex" }}>
         <Header/> 
 
-        <div>
+        <div className='Screen-sale'>
+          <h1> Realizar Venda </h1>
           <div className='Sale'>
-            <h1> Realizar Venda </h1>
+            {/* <h1> Realizar Venda </h1> */}
             
             <div className='Search'>
               <h5>Nome do Produto</h5>
@@ -61,9 +63,11 @@ function Vendas() {
                       <td> {product.sale}</td>
                       <td> {product.quantity}</td>
                       <td> 
-                        <button>
+                        
+                        <button type='button'>
                           {<FaPlus />}
                         </button>
+                        
                       </td>
                       {/* <td> 
                         <button>
@@ -79,6 +83,49 @@ function Vendas() {
         </div>
 
         <div className='Shop-cart'>
+          <h2>Carrinho de compras</h2>
+
+          <table className="table table-borderless ">
+              <thead>
+                <tr>
+                  {/* <th scope="col">ID</th> */}
+                  <th scope="col">Produto</th>
+                  <th scope="col">Preço</th>
+                  <th scope="col">Qtd</th>
+                  <th scope="col">Total</th>
+                  {/* <th scope="col">Retirar</th> */}
+
+                </tr>
+              </thead>
+              <tbody>
+
+                {products.map( (product,i) =>{
+                  return <tr key = {i+1}>
+                    {/* <th scope="row">{i+1}</th> */}
+                      <td> {product.name}</td>
+                      <td> {product.sale}</td>
+                      <td>
+                        <input type="text" defaultValue={1} 
+                        onChange={(e) => setpQtd(e.target.value)}
+                        />
+                      </td>
+                      <td>
+                        {pQtd}  
+                      </td>
+                   
+                    </tr> 
+                })}
+
+
+              </tbody>
+            </table>
+
+
+            <p>
+              Total: 
+            </p>
+            
+            <button type='button'>Finalizar</button>
           
         </div>
 
